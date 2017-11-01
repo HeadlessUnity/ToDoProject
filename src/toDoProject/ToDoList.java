d0ca2233fb535d4d2935318dca616bac71c43653
Charles Diep <charlesdiep91gmail.com>
2017-11-01 11:14:43 +0100

    Commiting again

--- src/toDoProject/ToDoList.java
+++ src/toDoProject/ToDoList.java
@@ -10,10 +10,11 @@ public class ToDoList {
 	
 	
 	
-	public void addTask(Task task) {
+	public void addTask(String title, String description) {
 		
 		//TODO: Add fucking check time date.
-		
+		Task task1 = new Task(Status.ACTIVE, title, description);
+		//tasklist<String>.add(task1);
 	}
 	
 	public void deleteTask(Task task) {
